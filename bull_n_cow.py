53a997e3b13770c996c6f6919824042ed14dae8c
GimGayeon <kky1373@naver.com>
2023-05-01 19:58:36 +0900

    feat: Create bull_n_cow.py
    This is create 4 number

---
+++ bull_n_cow.py
@@ -0,0 +1,10 @@
+import random #
+
+correct_number = ["0", "0", "0", "0"]
+
+correct_number[0] = str(random.randrange(1, 10, 1))
+correct_number[1] = str(random.randrange(1, 10, 1))
+correct_number[2] = str(random.randrange(1, 10, 1))
+correct_number[3] = str(random.randrange(1, 10, 1))
+
+
